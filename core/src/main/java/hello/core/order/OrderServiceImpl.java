a44f61b5bf8ac8e613858f5ba150a3e5af923383
dlawn <dlawnsrjs@gmail.com>
2022-07-13 18:49:05 +0900

    07-13 비즈니스 요구사항에 맞춘 회원 도메인 설계와 주문,할인 도메인 개발

---
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,26 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private DiscountPolicy discountPolicy;
+
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+
+        Member member = memberRepository.findById(memberId);
+        int discountPrice = discountPolicy.discount(member, itemPrice); //할인은 discountPolicy 너가 알아서해~ 단일책임원칙
+
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+
+
+    }
+}
