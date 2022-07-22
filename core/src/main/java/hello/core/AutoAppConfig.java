8a02be1fe7aeedd725b4ac0eea452be74ed36b48
dlawn <dlawnsrjs@gmail.com>
2022-07-22 19:08:53 +0900

    07-22 @ComponentScan과 Autowired를 이용한 자동 의존관계 주입

---
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,19 @@
+package hello.core;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration //설정정보
+@ComponentScan(
+        excludeFilters =  @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class) //해당 애노테이션을 뺌으로 원래 만들어놨던 AppConfig파일을 예외처리.
+) // 스프링 빈을 쫙 긁어서 자동으로 가져옴
+public class AutoAppConfig {
+
+
+
+
+
+
+}
