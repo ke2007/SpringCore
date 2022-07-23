7c7cd5bfae70528d2c09631e578dd7907c35c6b8
dlawn <dlawnsrjs@gmail.com>
2022-07-23 13:45:31 +0900

    07-23 @ComponentScan 탐색위치를 지정하는 방법

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration //설정정보
 @ComponentScan(
+        basePackages = "hello.core.member",       // hello.core.member만 컴포넌트 스캔의 대상이 된다.
         excludeFilters =  @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class) //해당 애노테이션을 뺌으로 원래 만들어놨던 AppConfig파일을 예외처리.
 ) // 스프링 빈을 쫙 긁어서 자동으로 가져옴
 public class AutoAppConfig {
