57dfc1412612cd769c9a9fc21b7406a291defc03
dlawn <dlawnsrjs@gmail.com>
2022-07-28 21:11:36 +0900

    07-28 Spring Bean의 LifeCycle을 알아보자 (feat. InitializingBean, DisposableBean)

--- core/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,12 +1,14 @@
 package hello.core.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
     private String url; //접속해야할 서버의 URL
 
     public NetworkClient() {
         System.out.println("생성자 호출 , url = " + url);
-        connect();
-        call("초기화 연결 메시지");
+
     }
 
 
@@ -32,5 +34,16 @@ public class NetworkClient {
 
     public void disconnect() {
         System.out.println("close " + url);
+    }
+
+    @Override //프로퍼티 세팅이 끝나면 ~ 실행
+    public void afterPropertiesSet() throws Exception {
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        disconnect();
     }
 }
